8a83f524b63062b77f0aad57297844d870018d70
vishwas patki <patki_vishwas@hotmail.com>
2013-03-09 20:33:32 +0530

    new php

---
+++ login.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<Title>Registration Form</Title>
+<style type="text/css">
+    body { background-color: #fff; border-top: solid 10px #000;
+        color: #333; font-size: .85em; margin: 20; padding: 20;
+        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+<form method="post" action="../../miniproject/index.php" enctype="multipart/form-data" >
+      Name  <input type="text" name="name" id="name"/></br>
+      Email <input type="text" name="email" id="email"/></br>
+      password <input type="password" name="password" id="password"/></br>
+      <input type="submit" name="submit" value="Submit" />
+</form>
+<?php
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+    // Connect to database.
+    try {
+          $conn = new PDO ( "sqlsrv:server = tcp:ctifbrdid2.database.windows.net,1433; Database = trial", "vishwas", "Myjoyisu18");        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+    if(!empty($_POST)) {
+    try {
+        $username = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        // Insert data
+        $sql_insert = "INSERT INTO trial (username, email, password) 
+                   VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $username);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $password);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    echo "<h3>Your're registered!</h3>";
+    }
+    // Retrieve data
+    $sql_select = "SELECT * FROM trial";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+    if(count($registrants) > 0) {
+        echo "<h2>People who are registered:</h2>";
+        echo "<table>";
+        echo "<tr><th>Name</th>";
+        echo "<th>Email</th>";
+        echo "<th>password</th></tr>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['username']."</td>";
+            echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['password']."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>No one is currently registered.</h3>";
+    }
+?>
+</body>
+</html>
+
