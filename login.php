c7cce3bd45811e50155e00e03169f611e80bb297
vishwas patki <patki_vishwas@hotmail.com>
2013-03-10 22:48:33 +0530

    reg

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ include "login.html";
 	  }
 	  else  if(count($registrant) ==0)
 	  {
-	  echo "enter values";
+	  echo "<legend>". Invalid email id or password . "</legend>";
    }
    else 
    {echo "error";
