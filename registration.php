19f4b651f66c5465ee1ce950d1947c61a84bcbfb
vishwas patki <patki_vishwas@hotmail.com>
2014-02-24 18:04:31 +0530

    vb

--- registration.php
+++ registration.php
@@ -1,18 +1,19 @@
 <?php
-include "registration.html";
+//include "registration.html";
     // DB connection info
     //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the portal.
     // Connect to database.
     try {
-          $conn = new PDO ( "sqlsrv:server = tcp:pocxo8zlbf.database.windows.net,1433; Database = classifieds", "sambaridly", "Butter@dosa112");      
+          $conn = new PDO ( "sqlsrv:server = tcp:pocxo8zlbf.database.windows.net,1433; Database =classifieds", "sambaridly", "Butter@dosa112");      
             $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            echo "success";
     }
     
     catch(Exception $e){
         die(var_dump($e));
     }
-    // Insert registration info
+    /*// Insert registration info
     if(!empty($_POST)) {
     try {
         $username = $_POST['username'];
@@ -63,5 +64,5 @@ include "registration.html";
     }
 	
 
-	
+	*/
 ?>
