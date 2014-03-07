dedf513c9c10eba59145c36da24ec3db720d3f5e
vishwas patki <patki_vishwas@hotmail.com>
2014-03-07 23:38:50 +0530

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include "default.html";
 try {
           $conn = new PDO ( "sqlsrv:server = tcp:pocxo8zlbf.database.windows.net,1433; Database =classifieds", "sambaridly", "Butter@dosa112");      
             $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -15,8 +16,25 @@ $sql_select = "SELECT * FROM adposts";
     $stmt = $conn->query($sql_select);
     $adposts = $stmt->fetchAll(); 
     if(count($adposts) > 0) {
-        echo "<h2>Ads posted</h2>";
+      echo "<div class='container-narrow'>";
+        echo "<div class='navbar navbar-inverse'>
+  <div class='navbar-inner'>
+    <a class='brand' href='default.html'>eZeefieds</a>
+      <ul class='nav navbar-nav'>
+        <li class=""><a href='default.html'>Home</a></li>
+        <li><a href='registration.html'>Create Account</a></li>
+        
+      </ul>
+      
+      <ul class='nav navbar-nav navbar-right'>
+        <li><a href='login.html'>Sign in</a></li>
+        <li><a href='postcategorypage.html'>Post ad</a></li>
+      </ul>
+    
+  </div>
+</div>";
         echo "<div class=panel panel-default>";
+        echo "<h2>Ads posted</h2>";
         echo "<table class='table'>";
         echo "<tr><th>Ad title</th>";
         echo "<th>Description</th>";
@@ -30,13 +48,15 @@ $sql_select = "SELECT * FROM adposts";
             echo "<td>".$adpost['price']."</td>";
             echo "<td>".$adpost['contact_name']."</td>";
             echo "<td>".$adpost['phoneno']."</td>";
-            echo "<td>".$registrant['location']."</td></tr>";
+            echo "<td>".$adpost['location']."</td></tr>";
         }
         echo "</table>";
         echo "</div>";
     } else {
         echo "<h3>No one is currently registered.</h3>";
+
     }
+    echo "</div>";
 
 
 ?>
