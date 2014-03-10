2064e2d63e80709e2e84b320c31157f03c22fb80
vishwas patki <patki_vishwas@hotmail.com>
2014-03-10 21:28:47 +0530

    img

--- uploadimage.php
+++ uploadimage.php
@@ -12,14 +12,61 @@ include "uploadimage.html";
         die(var_dump($e));
     }
 $image=$_POST['image'];
+$target_path = "http://icards.azurewebsites.net/".$image;
+
+$target_path = $target_path . basename( $_FILES['file']['name']);
  $sql_insert = "INSERT INTO images (image) 
                    VALUES (?)";
         $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $image);
+        $stmt->bindValue(1, $target_path);
          $stmt->execute();
        
 
 
 
 
+?>
+
+<?php
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      
+      //echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
 ?>
