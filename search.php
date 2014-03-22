5049615033bf822b8844409b1ddeb99e23d5672a
vishwas patki <patki_vishwas@hotmail.com>
2014-03-22 11:26:23 +0530

    hfdjs

--- search.php
+++ search.php
@@ -11,11 +11,10 @@ try {
     catch(Exception $e){
         die(var_dump($e));
     }
-$sortby=$_POST['sortby']
-$category=$_GET['id'];
 
+$category=$_GET['id'];
 echo "<link href='css/bootstrap.css' rel='stylesheet' />";
-     $sql_select = "SELECT * FROM adposts where choosen_category='$category'";
+    $sql_select = "SELECT * FROM adposts where choosen_category='$category'";
     $stmt = $conn->query($sql_select);
     $adposts = $stmt->fetchAll(); 
     if(count($adposts) > 0) {
@@ -41,68 +40,8 @@ echo "<link href='css/bootstrap.css' rel='stylesheet' />";
         echo "</table>";
         echo "</div>";
         echo "</div>";
-    } 
-    if($sortby=='low_to_high'){
-    $sql_select = "SELECT * FROM adposts where choosen_category='$category'and ORDER BY price ASC";
-    $stmt = $conn->query($sql_select);
-    $adposts = $stmt->fetchAll(); 
-    if(count($adposts) > 0) {
-        echo "<div class=container-narrow>";
-        echo "<div class=panel panel-default>";
-        echo "<div style=height:50px></div>";
-        echo "<legend>Ads posted</legend>";
-        echo "<table class='table table-bordered'>";
-        echo "<tr><th>Ad title</th>";
-        echo "<th>Description</th>";
-        echo "<th>Price</th>";
-        echo "<th>Seller's name</th>";
-        echo "<th>Phone no</th>";
-        echo "<th>Location</th></tr>";
-        foreach($adposts as $adpost) {
-            echo "<tr><td>".$adpost['adtitle']."</td>";
-            echo "<td>".$adpost['description']."</td>";
-            echo "<td>".$adpost['price']."</td>";
-            echo "<td>".$adpost['contact_name']."</td>";
-            echo "<td>".$adpost['phoneno']."</td>";
-            echo "<td>".$adpost['location']."</td></tr>";
-        }
-        echo "</table>";
-        echo "</div>";
-        echo "</div>";
-     } 
-    }
-    else if($sortby=='high_to_low'){
-    $sql_select = "SELECT * FROM adposts where choosen_category='$category'and ORDER BY price DESC";
-    $stmt = $conn->query($sql_select);
-    $adposts = $stmt->fetchAll(); 
-    if(count($adposts) > 0) {
-        echo "<div class=container-narrow>";
-        echo "<div class=panel panel-default>";
-        echo "<div style=height:50px></div>";
-        echo "<legend>Ads posted</legend>";
-        echo "<table class='table table-bordered'>";
-        echo "<tr><th>Ad title</th>";
-        echo "<th>Description</th>";
-        echo "<th>Price</th>";
-        echo "<th>Seller's name</th>";
-        echo "<th>Phone no</th>";
-        echo "<th>Location</th></tr>";
-        foreach($adposts as $adpost) {
-            echo "<tr><td>".$adpost['adtitle']."</td>";
-            echo "<td>".$adpost['description']."</td>";
-            echo "<td>".$adpost['price']."</td>";
-            echo "<td>".$adpost['contact_name']."</td>";
-            echo "<td>".$adpost['phoneno']."</td>";
-            echo "<td>".$adpost['location']."</td></tr>";
-        }
-        echo "</table>";
-        echo "</div>";
-        echo "</div>";
-      } 
-    }
-    else{
-   
-    
+    } else {
+        echo "<h3></h3>";
     }
 echo "</body>";
 
