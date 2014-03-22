d0bb1ecf74481b5859ae5118c3e6a05bad701623
vishwas patki <patki_vishwas@hotmail.com>
2014-03-22 16:51:51 +0530

    css

--- search.php
+++ search.php
@@ -19,10 +19,10 @@ try {
     echo "<div class=panel panel-default>";
     echo "<div style=height:50px></div>";
     echo "<legend>Ads related to :  ".$category."</legend>";
-    if($sortby=='low_to_high'){
+    if($sortby==low_to_high){
         $sql_select = "SELECT * FROM adposts where choosen_category='$category' OR choosen_category='$category_id' ORDER BY price ASC";
     }
-    elseif ($sortby=='high_to_low') {
+    elseif ($sortby==high_to_low) {
         $sql_select = "SELECT * FROM adposts where choosen_category='$category' OR choosen_category='$category_id' ORDER BY price DESC";
     }
     
