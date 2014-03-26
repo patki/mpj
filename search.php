d2b77410f361632300b9805685a646558f34c863
vishwas patki <patki_vishwas@hotmail.com>
2014-03-26 19:51:37 +0530

    timedate

--- search.php
+++ search.php
@@ -19,7 +19,7 @@ try {
     echo "<div class=panel panel-default>";
     echo "<div style=height:50px></div>";
     echo "<legend>Ads related to :  ".$category."</legend>";
-    $sql_select = "SELECT * FROM adposts where choosen_category='$category' OR choosen_category='$category_id'";
+    $sql_select = "SELECT * FROM adposts where choosen_category='$category' OR choosen_category='$category_id' ORDER BY timedate DESC";
     if($sortby==low_to_high){
         /*$sql_select = "SELECT * FROM adposts where choosen_category='$category' OR choosen_category='$category_id' ORDER BY price ASC";*/
         $sql_select = "SELECT * FROM adposts  ORDER BY price ASC";
